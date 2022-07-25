a08fbca1c8046c06edd2c57d2db34c0a565dd991
DESKTOP-4800U\GOD 4800U <dockeylev@gmail.com>
2022-07-25 16:25:54 +0200

    TODO v21 Add PreAuthorize to HomeController

--- src/main/java/com/dockey/todoapi/controllers/HomeController.java
+++ src/main/java/com/dockey/todoapi/controllers/HomeController.java
@@ -7,6 +7,7 @@ import com.dockey.todoapi.entities.User;
 import com.dockey.todoapi.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
@@ -62,6 +63,7 @@ public class HomeController {
         return "register_success";
     }
 
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     @GetMapping("/users")  // LIST USERS
     public String listUsers(Model model, Authentication authentication) {
         List<User> listUsers = service.listAll();
@@ -71,9 +73,10 @@ public class HomeController {
         return "users";
     }
 
-    @GetMapping("/users/edit/{id}")  // EDIT USER
-    public String editUser(@PathVariable("id") Long id, Model model, Authentication authentication) {
-        User user = service.get(id);
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN') or @authenticatedUserService.hasId(#userId)")
+    @GetMapping("/users/edit/{userId}")  // EDIT USER
+    public String editUser(@PathVariable("userId") Long userId, Model model, Authentication authentication) {
+        User user = service.get(userId);
         List<Role> listRoles = service.listRoles();
         model.addAttribute("user", user);
         model.addAttribute("listRoles", listRoles);
@@ -82,53 +85,59 @@ public class HomeController {
         return "user_form";
     }
 
-    @DeleteMapping("/users/{id}")  // DELETE USER
-    public void deleteUser(@PathVariable("id") Long id, Authentication authentication) {
-        User user = service.get(id);
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN') or @authenticatedUserService.hasId(#userId)")
+    @DeleteMapping("/users/{userId}")  // DELETE USER
+    public void deleteUser(@PathVariable("userId") Long userId, Authentication authentication) {
+        User user = service.get(userId);
         log.info("Logged user: " + authentication.getName() + " User: " + user.getUsername() + " / Delete user");
-        service.removeUser(id);
+        service.removeUser(userId);
     }
 
-    @PostMapping("/users/save")  // SAVE USER
-    public RedirectView saveUser(User user, Authentication authentication) throws IOException {
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN') or @authenticatedUserService.hasId(#userId)")
+    @PostMapping("/users/{userId}/save")  // SAVE USER
+    public RedirectView saveUser(@PathVariable("userId") Long userId, User user, Authentication authentication) throws IOException {
         User savedUser = service.save(user);
         log.info("Logged user: " + authentication.getName() + " User: " + savedUser.getUsername() + " / Save user / " + savedUser);
         return new RedirectView("/users", true);
     }
 
-    @GetMapping("/users/{id}/todos")  // LIST TODOS
-    public String listTodo(@PathVariable("id") Long id, Model model, Authentication authentication) {
-        List<Todo> listTodos = service.listTodos(id);
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN') or @authenticatedUserService.hasId(#userId)")
+    @GetMapping("/users/{userId}/todos")  // LIST TODOS
+    public String listTodo(@PathVariable("userId") Long userId, Model model, Authentication authentication) {
+        List<Todo> listTodos = service.listTodos(userId);
         model.addAttribute("listTodos", listTodos);
-        model.addAttribute("todoGid", id);
-        User user = service.get(id);
+        model.addAttribute("todoGid", userId);
+        User user = service.get(userId);
         log.info("Logged user: " + authentication.getName() + " User: " + user.getUsername() + " / List todos");
         return "todos";
     }
 
-    @GetMapping("/users/todo/{todoId}")  // EDIT TODO
-    public String editTodo(@PathVariable("todoId") Long todoId, Model model, Authentication authentication) {
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN') or @authenticatedUserService.hasId(#userId)")
+    @GetMapping("/users/{userId}/todo/{todoId}")  // EDIT TODO
+    public String editTodo(@PathVariable("userId") Long userId, @PathVariable("todoId") Long todoId, Model model, Authentication authentication) {
         Todo todo = todoRepository.findById(todoId).get();
         model.addAttribute("todo", todo);
-        User user = service.get(todo.getGid());
+        User user = service.get(userId);
         log.info("Logged user: " + authentication.getName() + " User: " + user.getUsername() + " / Edit todo");
         return "todo_form";
     }
 
-    @PostMapping("/users/todo/save")  // SAVE TODO
-    public String saveUser(Todo todo, Authentication authentication) {
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN') or @authenticatedUserService.hasId(#userId)")
+    @PostMapping("/users/{userId}/todo/save")  // SAVE TODO
+    public String saveUser(@PathVariable("userId") Long userId, Todo todo, Authentication authentication) {
         todoRepository.save(todo);
-        User user = service.get(todo.getGid());
+        User user = service.get(userId);
         log.info("Logged user: " + authentication.getName() + " User: " + user.getUsername() + " / Save todo / " + todo);
         return "redirect:/users/" + todo.getGid() + "/todos";
     }
 
-    @GetMapping("/users/{todoGid}/newtodo")  // NEW TODO
-    public String newTodo(@PathVariable("todoGid") Long todoGid, Model model, Authentication authentication) {
+    @PreAuthorize(value = "hasRole('ROLE_ADMIN') or @authenticatedUserService.hasId(#userId)")
+    @GetMapping("/users/{userId}/newtodo")  // NEW TODO
+    public String newTodo(@PathVariable("userId") Long userId, Model model, Authentication authentication) {
         Todo todo = new Todo();
-        todo.setGid(todoGid);
+        todo.setGid(userId);
         model.addAttribute("todo", todo);
-        User user = service.get(todo.getGid());
+        User user = service.get(userId);
         log.info("Logged user: " + authentication.getName() + " User: " + user.getUsername() + " / New todo");
         return "todo_form";
     }
